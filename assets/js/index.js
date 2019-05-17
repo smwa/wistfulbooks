02bcfbe0af28f57ce7ca59a07986abe53f079f26
Michael Smith <michael.smith.ok@gmail.com>
2019-05-16 21:56:15 -0500

    Working towards download button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    var startButtonGroup = "<div class='btn-group' role='group' aria-label='Book actions'>";
+    var infoButton = "<button type='button' title='Info' aria-label='Show book information' class='book_info btn btn-outline-dark'>&#8505;</button>";
+    var playButton = "<button type='button' title='Play' aria-label='Play book' class='play_book btn btn-outline-dark'>&#9654;</button>";
+    var storeOfflineButton = "<button type='button' title='Download for offline use' aria-label='Download for offline use' class='download_book btn btn-outline-dark'>Store Offline</button>";
+    var isStoredOfflineButton = "<button type='button' title='Book is available offline' aria-label='Book is available offline' class='downloaded_book btn btn-outline-dark' disabled>Available Offline</button>";
+    var endButtonGroup = '</div>';
     var t = $('#books').DataTable({
         'ajax': 'catalog/catalog.json',
         'deferRender': true,
@@ -40,10 +46,11 @@ $(document).ready(function() {
                 'searchable': false,
                 'render': function(bookPath) {
                     return "<input type='hidden' class='book_path' value='" + bookPath + "'>"
-                         + "<div class='btn-group' role='group' aria-label='Book actions'>"
-                         + "<button type='button' title='Info' aria-label='Show book information' class='book_info btn btn-outline-dark'>&#8505;</button>"
-                         + "<button type='button' title='Play' aria-label='Play book' class='play_book btn btn-outline-dark'>&#9654;</button>"
-                         + "</div>";
+                         + startButtonGroup
+                         + infoButton
+                         + playButton
+                         + (false ? isStoredOfflineButton : storeOfflineButton) // TODO Change true to check if book is available offline
+                         + endButtonGroup;
                 }
             }
         ]
@@ -99,6 +106,10 @@ $(document).ready(function() {
 
         APlayerObject.play();
     });
+  });
+
+  $(document).on('click', '.download_book', function (ev) {
+    alert('TODO Download book'); // TODO Download book
   });
 
   var APlayerObject = new APlayer({
